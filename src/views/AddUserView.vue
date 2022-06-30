913cda0aadec92ab1864d9fb32ece5a91a24a875
Colin Gregurich <coling112198@gmail.com>
2022-06-30 17:55:44 -0500

    mreo stuff man

--- src/views/AddUserView.vue
+++ src/views/AddUserView.vue
@@ -1,18 +1,47 @@
 <template>
     <div class="add-user-view">
         <label for="username">Username
-            <input name="username" type="text">
+            <input type="text" v-model="username">
         </label>
-        <button type="submit">Add User</button>
+        <label for="password">Password
+          <input type="text" v-model="password">
+        </label>
+        <label for="email">Email
+          <input type="text" v-model="email">
+        </label>
+        <button type="submit" @click="submitClicked">Add User</button>
     </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      username: '',
+      password: '',
+      email: ''
+    };
+  },
+  methods: {
+    async submitClicked() {
+      const body = {username: this.username, password: this.password, email: this.email};
+      const res = await fetch('http://localhost:5000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      });
+      console.log(res);
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
+.add-user-view {
+  display: flex;
+  flex-direction: column;
+}
 
 </style>
--- src/views/ShowUsersView.vue
+++ src/views/ShowUsersView.vue
@@ -3,10 +3,18 @@
     Show Users View
     <div class="users">
       <div class="user" v-for="user in users" :key="user.id">
-        <div>Username: {{ user.username }}</div>
         <div>ID: {{ user.id }}</div>
+        <div>Username: {{ user.username }}</div>
+        <div>Email: {{ user.email }}</div>
         <hr>
       </div>
+    </div>
+    <h3>Fake Login</h3>
+    <div class="fake-login">
+      <label for="username">Username<input v-model="enteredUsername" type="text" name="username"></label>
+      <label for="password">Password<input v-model="enteredPassword" type="text" name="password"></label>
+      <div class="login-response">{{ loginResponse }}</div>
+      <button type="submit" @click="login">"Login"</button>
     </div>
   </div>
 </template>
@@ -20,9 +28,27 @@ export default {
   },
   data() {
     return {
-      users: []
+      users: [],
+      enteredUsername: '',
+      enteredPassword: '',
+      loginResponse: 'login response',
     };
-  }
+  },
+  methods: {
+    async login() {
+      console.log('login');
+      const body = {username: this.enteredUsername, password: this.enteredPassword};
+      const res = await fetch('http://localhost:5000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      });
+      const isSuccess = (await res.json()).success;
+      this.loginResponse = isSuccess ? 'Success!' : 'Failure';
+    }
+  },
 };
 </script>
 
