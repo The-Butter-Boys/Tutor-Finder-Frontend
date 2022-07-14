abf4a7f90a2f85fb047ed72c7b1bac538b3040cc
Carter Gamary <93697291+gcarter85@users.noreply.github.com>
2022-07-14 13:05:48 -0500

    Update AddUserView.spec.js
    Clerical fix

--- tests/unit/AddUserView.spec.js
+++ tests/unit/AddUserView.spec.js
@@ -4,7 +4,7 @@ import {shallowMount} from "@vue/test-utils";
 describe("AddUserView.vue", ()=> {
   it("is returned", async () => {
     const mockMethod = jest.spyOn(AddUsersView.methods, 'submitClicked');
-    await shallowMount(ShowUsersView).find('button').trigger('click');
+    await shallowMount(AddUserView).find('button').trigger('click');
     expect(mockMethod).toHaveBeenCalled();
   })
 
